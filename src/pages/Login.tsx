555dcdc2351ebfb885092fd216d37b3b1856e702
İbrahim Doğruer <ibrahim@assayazilim.com>
2022-04-05 14:56:59 +0300

    email validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../contexts/AuthProvider';
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { toast } from 'react-toastify';
+import validator from 'validator'
 
 const Login = () => {
     const authContext = useContext(AuthContext);
@@ -13,20 +14,37 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [emailError, setEmailError] = useState('')
+    const validateEmail = (email) => {
+        setEmail(email)
+
+        if (validator.isEmail(email)) {
+            setEmailError('');
+        } else {
+            setEmailError('Email Invalid!')
+        }
+    }
+
+    const isFormInvalid = (): boolean => {
+        return email === '' || !validator.isEmail(email) || password === '';
+    }
+
     const login = async (e) => {
         e.preventDefault();
 
-        await signInWithEmailAndPassword(auth, email, password)
-            .then(res => {
-                authContext.setUser(res.user);
-                console.log(res, 'res')
-                toast.success('Login successful');
-                navigate("/");
-            })
-            .catch(error => {
-                console.log(error.message);
-                toast.error(error.message);
-            });
+        if (!isFormInvalid()) {
+            await signInWithEmailAndPassword(auth, email, password)
+                .then(res => {
+                    authContext.setUser(res.user);
+                    console.log(res, 'res')
+                    toast.success('Login successful');
+                    navigate("/");
+                })
+                .catch(error => {
+                    console.log(error.message);
+                    toast.error(error.message);
+                });
+        }
     }
 
     return (
@@ -38,14 +56,19 @@ const Login = () => {
                         <div className="form-group mt-4">
                             <label>Email</label>
                             <input type="email" className="form-control shadow" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter Email"
-                                onChange={(e) => setEmail(e.target.value)} value={email} />
+                                onChange={(e) => validateEmail(e.target.value)} value={email} />
+                            {emailError !== '' &&
+                                <span className="text-danger small ms-2">
+                                    {emailError}
+                                </span>
+                            }
                         </div>
                         <div className="form-group mt-4">
                             <label>Password</label>
                             <input type="password" className="form-control shadow" id="inputPassword" placeholder="Password"
                                 onChange={(e) => setPassword(e.target.value)} value={password} />
                         </div>
-                        <button type="submit" className="btn btn-primary shadow mt-4">Login</button>
+                        <button type="submit" className="btn btn-primary shadow mt-4" disabled={isFormInvalid()}>Login</button>
                         <p className="mt-3">Create an account <Link to={{ pathname: '/signup', }}>Signup</Link></p>
                     </form>
                 </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,6 +8,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth'
 import firebaseAuth from 'firebase/auth';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom'
+import validator from 'validator'
 
 const Register = () => {
     const authContext = useContext(AuthContext);
@@ -17,29 +18,47 @@ const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const [emailError, setEmailError] = useState('')
+    const validateEmail = (e) => {
+        var email = e.target.value
+        setEmail(email)
+
+        if (validator.isEmail(email)) {
+            setEmailError('');
+        } else {
+            setEmailError('Email Invalid!')
+        }
+    }
+
+    const isFormInvalid = (): boolean => {
+        return email === '' || !validator.isEmail(email) || username === '' || password === '';
+    }
+
     const register = async (e) => {
         e.preventDefault();
 
-        await createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential: firebaseAuth.UserCredential) => {
-                authContext.setUser(userCredential);
-                console.log('userCredential', userCredential);
+        if (!isFormInvalid()) {
+            await createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential: firebaseAuth.UserCredential) => {
+                    authContext.setUser(userCredential.user);
+                    console.log('userCredential', userCredential);
 
-                setDoc(doc(db, "Users", userCredential.user!.uid), {
-                    email: email,
-                    username: username,
-                }).then(() => {
-                    console.log('User created');
-                    toast.success('You successfully register');
-                    navigate("/");
+                    setDoc(doc(db, "Users", userCredential.user!.uid), {
+                        email: email,
+                        username: username,
+                    }).then(() => {
+                        console.log('User created');
+                        toast.success('You successfully register');
+                        navigate("/");
+                    }).catch(error => {
+                        console.log(error.message);
+                        toast.error(error.message);
+                    });
                 }).catch(error => {
                     console.log(error.message);
                     toast.error(error.message);
                 });
-            }).catch(error => {
-                console.log(error.message);
-                toast.error(error.message);
-            });
+        }
     }
 
     return (
@@ -51,19 +70,24 @@ const Register = () => {
                         <div className="form-group mt-4">
                             <label>Email</label>
                             <input type="email" className="form-control shadow" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter Email"
-                                onChange={(e) => setEmail(e.target.value)} value={email} />
+                                onChange={(e) => validateEmail(e)} value={email} required />
+                            {emailError !== '' &&
+                                <span className="text-danger small ms-2">
+                                    {emailError}
+                                </span>
+                            }
                         </div>
                         <div className="form-group mt-4">
                             <label>Username</label>
                             <input type="text" className="form-control shadow" id="inputUsername" aria-describedby="usernameHelp" placeholder="Enter Username"
-                                onChange={(e) => setUsername(e.target.value)} value={username} />
+                                onChange={(e) => setUsername(e.target.value)} value={username} required />
                         </div>
                         <div className="form-group mt-4">
                             <label >Password</label>
                             <input type="password" className="form-control shadow" id="inputPassword" placeholder="Password"
-                                onChange={(e) => setPassword(e.target.value)} value={password} />
+                                onChange={(e) => setPassword(e.target.value)} value={password} required />
                         </div>
-                        <button type="submit" className="btn btn-primary shadow mt-4">Signup</button>
+                        <button type="submit" className="btn btn-primary shadow mt-4" disabled={isFormInvalid()}>Signup</button>
                         <p className="mt-3">Already have an account <Link to={{ pathname: '/login', }}>Login</Link></p>
                     </form>
                 </div>
